c0881873a573066746c00961ca2ea3eb73d30d42
prasetion <prasetion@gmail.com>
2018-04-24 21:00:06 +0700

    initial commit
    init commit

---
+++ Assets/Scripts/EventDelegateHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventDelegateHandler : MonoBehaviour {
+
+	public delegate void BroadcastEvent();
+	public event BroadcastEvent OnSetScore;
+	public event BroadcastEvent OnGetScore;
+
+	public delegate void BroadcastMessage(string message);
+	public event BroadcastMessage OnShareStatus;
+
+	void Start(){
+
+		if(OnSetScore != null)
+			OnSetScore();
+		
+		if(OnGetScore != null)
+			OnGetScore();
+
+		if(OnShareStatus != null)
+			OnShareStatus("Ini statusnya");
+	}
+
+	void Init(){
+
+	}
+
+}
---
+++ Assets/Scripts/EventReciever.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventReciever : MonoBehaviour {
+
+	public EventDelegateHandler _EventDelegateHandler;
+
+	// Use this for initialization
+	void Start () {
+		_EventDelegateHandler.OnGetScore += GetScore;
+		_EventDelegateHandler.OnSetScore += SetScore;
+		_EventDelegateHandler.OnShareStatus += YourStatus;
+	}
+
+	void OnDestroy(){
+		_EventDelegateHandler.OnGetScore -= GetScore;
+		_EventDelegateHandler.OnSetScore -= SetScore;
+		_EventDelegateHandler.OnShareStatus -= YourStatus;
+	}
+
+	void GetScore(){
+		Debug.Log("Get Score");
+	}
+
+	void SetScore(){
+		Debug.Log("Set Score");
+	}
+
+	void YourStatus(string status){
+		Debug.Log(status);
+	}
+
+}
